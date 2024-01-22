5cc1056c1becfdb4b37fa4ae6ed2e034feb6cb42
Michael Rosenberg <michael@mrosenberg.pub>
2024-01-21 21:36:13 -0500

    Added sript to build benchmarks for my router

---
+++ latke/build_router_bench.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# This requires the aarch64 musl target to be installed:
+#     rustup target add aarch64-unknown-linux-musl
+# It also requires a musl cross compiler, which can be set up on macOS using this repo
+#     https://github.com/FiloSottile/homebrew-musl-cross
+# The change to the cargo config is necessary.
+
+cargo bench --no-run --target aarch64-unknown-linux-musl -- -C target-cpu=cortex-a53
